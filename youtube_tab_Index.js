578788952cf4355512301abdc8c8a7aaba046881
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-08-08 16:52:08 +0900

    remove navbar & add style once

--- youtube_tab_Index.js
+++ youtube_tab_Index.js
@@ -1,109 +1,125 @@
-    
-    var newStyle = document.createElement('style');
-    newStyle.type = 'text/css';
-    newStyle.innerText = 'a:focus{outline: none; border-style: solid; border-width: 7px; border-color: #E71D36;}';
-    document.getElementsByTagName('HEAD').item(0).appendChild(newStyle);
 
-    document.getElementById('chips-wrapper').style.display = "none";
+// 左のnavバーを非表示
+var navBar = document.querySelector("#guide");
+if (navBar) {
+    navBar.remove();
+}
+//navバーのあった場所の空白を取り除く
+document.querySelector("#page-manager").setAttribute("style", "margin-left: 0px;");
 
-    tabIndex();
+//検索窓の下のタグ一覧を非表示
+document.getElementById('chips-wrapper').style.display = "none";
 
-    gridUx();
+tabIndex();
 
-    var unnecessaryContents = document.querySelectorAll("ytd-rich-section-renderer");
-    if (unnecessaryContents) {
-        unnecessaryContents.forEach(node => {
-            node.style.display = "none";
-        });
-    };
 
-    document.querySelector("#contents").querySelector("#thumbnail").focus();
+//コロナ特集＆急上昇特集を非表示
+var unnecessaryContents = document.querySelectorAll("ytd-rich-section-renderer");
+if (unnecessaryContents) {
+    unnecessaryContents.forEach(node => {
+        node.style.display = "none";
+    });
+};
 
-    function gridUx() {
-        const EVENT_LISTENER_ADDED = "eventListenerAdded";
-        if (document.querySelectorAll("#primary")[0].classList.contains(EVENT_LISTENER_ADDED)) {
-            return;
-        } else {
-            document.querySelectorAll("#primary")[0].classList.add(EVENT_LISTENER_ADDED);
+document.querySelector("#contents").querySelector("#thumbnail").focus();
 
-            var i = 0;
+//return;使うために関数に入れる
+(function () {
+    //２回目以降の実行はスルーする為にクラスリスト追加＆監視
+    const EVENT_LISTENER_ADDED = "eventListenerAdded";
+    if (document.querySelectorAll("#primary")[0].classList.contains(EVENT_LISTENER_ADDED)) {
+        return;
+    } else {
+        document.querySelectorAll("#primary")[0].classList.add(EVENT_LISTENER_ADDED);
 
-            document.querySelectorAll("#primary")[0].addEventListener("keydown", function (event) {
-                var num = document.querySelectorAll("#primary")[0].firstElementChild.getAttribute("style").split(":")[1].slice(0, 1);
-                var nodes = document.querySelectorAll('.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail');
-                nodes = Array.from(nodes);
-                thumbnails = nodes.filter(node => {
-                    return node.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer");
-                });
+        //css追加　フォーカスの時の外枠
+        var newStyle = document.createElement('style');
+        newStyle.type = 'text/css';
+        newStyle.innerText = 'a:focus{outline: none; border-style: solid; border-width: 7px; border-color: #E71D36;}';
+        document.getElementsByTagName('HEAD').item(0).appendChild(newStyle);
 
-                var activeThumbnail = thumbnails.find(node => node === document.activeElement);
-                var currentIndex = thumbnails.indexOf(activeThumbnail);
+        //サムネの親要素にキー入力を受け付けるイベントリスナーを設置
+        var i = 0;
+        document.querySelectorAll("#primary")[0].addEventListener("keydown", function (event) {
+            //列ごとの動画数を取得 num 列ごとの数が変わっても真上や真下の要素にアクセスするため
+            var num = document.querySelectorAll("#primary")[0].firstElementChild.getAttribute("style").split(":")[1].slice(0, 1);
+            var nodes = document.querySelectorAll('.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail');
+            nodes = Array.from(nodes);
+            thumbnails = nodes.filter(node => {
+                return node.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer");
+            });
 
-                if (event.key === "w") {
-                    if (thumbnails[parseInt(i) - parseInt(num)]) {
-                        i = parseInt(currentIndex) - parseInt(num);
-                        thumbnails[i].focus();
-                    };
+            var activeThumbnail = thumbnails.find(node => node === document.activeElement);
+            var currentIndex = thumbnails.indexOf(activeThumbnail);
 
-                } else if (event.key === "s") {
-                    if (thumbnails[parseInt(i) + parseInt(num)]) {
-                        i = parseInt(currentIndex) + parseInt(num);
-                        thumbnails[i].focus();
-                    };
-
-                } else if (event.key === "a") {
-                    if (thumbnails[parseInt(i) - 1]) {
-                        i = parseInt(currentIndex) - 1;
-                        thumbnails[i].focus();
-                    };
-
-                } else if (event.key === "d") {
-                    if (thumbnails[parseInt(i) + 1]) {
-                        i = parseInt(currentIndex) + 1;
-                        thumbnails[i].focus();
-                    };
+            if (event.key === "w") {
+                if (thumbnails[parseInt(i) - parseInt(num)]) {
+                    i = parseInt(currentIndex) - parseInt(num);
+                    thumbnails[i].focus();
                 };
-            }, true);
-        }
-    };
 
-    function tabIndex() {
+            } else if (event.key === "s") {
+                if (thumbnails[parseInt(i) + parseInt(num)]) {
+                    i = parseInt(currentIndex) + parseInt(num);
+                    thumbnails[i].focus();
+                };
 
-        var nodes = document.querySelectorAll('.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail');
-        nodes = Array.from(nodes);
-        thumbnails = nodes.filter(node => {
-            return node.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer");
-        });
+            } else if (event.key === "a") {
+                if (thumbnails[parseInt(i) - 1]) {
+                    i = parseInt(currentIndex) - 1;
+                    thumbnails[i].focus();
+                };
 
-        thumbnails.forEach(thumbnail => {
-            if (thumbnail.getAttribute("tabindex") === "1") {
-                return;
-            } else {
-                thumbnail.setAttribute("tabindex", "1");
-                thumbnail.addEventListener('focus', () => {
-                    document.activeElement.scrollIntoView({
-                        behavior: "instant",
-                        block: "center"
-                    });
-                });
-            }
-        });
+            } else if (event.key === "d") {
+                if (thumbnails[parseInt(i) + 1]) {
+                    i = parseInt(currentIndex) + 1;
+                    thumbnails[i].focus();
+                };
+            };
+        }, true);
     }
-    const target = document.getElementById('contents');
-    const observer = new MutationObserver(records => {
-        tabIndex();
+}());
+
+//サムネ一つ一つにtabindexを追加する関数
+function tabIndex() {
+    var nodes = document.querySelectorAll('.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail');
+    nodes = Array.from(nodes);
+    thumbnails = nodes.filter(node => {
+        return node.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer");
     });
 
-    observer.observe(target, {
-        childList: true
-    })
+    thumbnails.forEach(thumbnail => {
+        if (thumbnail.getAttribute("tabindex") === "1") {
+            return;
+        } else {
+            thumbnail.setAttribute("tabindex", "1");
+            thumbnail.addEventListener('focus', () => {
+                document.activeElement.scrollIntoView({
+                    behavior: "instant",
+                    block: "center"
+                });
+            });
+        }
+    });
+}
+
+//サムネの親要素のDOM変化を監視、コンテンツが新たに読み込まれたらtabindexを実行
+const target = document.getElementById('contents');
+const observer = new MutationObserver(records => {
+    tabIndex();
+});
+
+observer.observe(target, {
+    childList: true
+})
 
 
 //フォーマット&ブックマークレット化したやつ
 javascript: (() => {
-var e=document.createElement("style");e.type="text/css";e.innerText="a:focus{outline: none; border-style: solid; border-width: 7px; border-color: #E71D36;}";document.getElementsByTagName("HEAD").item(0).appendChild(e);document.getElementById("chips-wrapper").style.display="none";g();h();var k=document.querySelectorAll("ytd-rich-section-renderer");k&&k.forEach(function(a){a.style.display="none"});document.querySelector("#contents").querySelector("#thumbnail").focus();
-function h(){if(!document.querySelectorAll("#primary")[0].classList.contains("eventListenerAdded")){document.querySelectorAll("#primary")[0].classList.add("eventListenerAdded");var a=0;document.querySelectorAll("#primary")[0].addEventListener("keydown",function(b){var d=document.querySelectorAll("#primary")[0].firstElementChild.getAttribute("style").split(":")[1].slice(0,1),c=document.querySelectorAll(".yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail");c=Array.from(c);thumbnails=c.filter(function(f){return f.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer")});
-c=thumbnails.find(function(f){return f===document.activeElement});c=thumbnails.indexOf(c);"w"===b.key?thumbnails[parseInt(a)-parseInt(d)]&&(a=parseInt(c)-parseInt(d),thumbnails[a].focus()):"s"===b.key?thumbnails[parseInt(a)+parseInt(d)]&&(a=parseInt(c)+parseInt(d),thumbnails[a].focus()):"a"===b.key?thumbnails[parseInt(a)-1]&&(a=parseInt(c)-1,thumbnails[a].focus()):"d"===b.key&&thumbnails[parseInt(a)+1]&&(a=parseInt(c)+1,thumbnails[a].focus())},!0)}}
-function g(){var a=document.querySelectorAll(".yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail");a=Array.from(a);thumbnails=a.filter(function(b){return b.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer")});thumbnails.forEach(function(b){"1"!==b.getAttribute("tabindex")&&(b.setAttribute("tabindex","1"),b.addEventListener("focus",function(){document.activeElement.scrollIntoView({behavior:"instant",block:"center"})}))})}var l=document.getElementById("contents");
-(new MutationObserver(function(){g()})).observe(l,{childList:!0});
+var g=document.querySelector("#guide");g&&g.remove();document.querySelector("#page-manager").setAttribute("style","margin-left: 0px;");document.getElementById("chips-wrapper").style.display="none";h();var k=document.querySelectorAll("ytd-rich-section-renderer");k&&k.forEach(function(c){c.style.display="none"});document.querySelector("#contents").querySelector("#thumbnail").focus();
+(function(){if(!document.querySelectorAll("#primary")[0].classList.contains("eventListenerAdded")){document.querySelectorAll("#primary")[0].classList.add("eventListenerAdded");var c=document.createElement("style");c.type="text/css";c.innerText="a:focus{outline: none; border-style: solid; border-width: 7px; border-color: #E71D36;}";document.getElementsByTagName("HEAD").item(0).appendChild(c);var a=0;document.querySelectorAll("#primary")[0].addEventListener("keydown",function(d){var e=document.querySelectorAll("#primary")[0].firstElementChild.getAttribute("style").split(":")[1].slice(0,
+1),b=document.querySelectorAll(".yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail");b=Array.from(b);thumbnails=b.filter(function(f){return f.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer")});b=thumbnails.find(function(f){return f===document.activeElement});b=thumbnails.indexOf(b);"w"===d.key?thumbnails[parseInt(a)-parseInt(e)]&&(a=parseInt(b)-parseInt(e),thumbnails[a].focus()):"s"===d.key?thumbnails[parseInt(a)+parseInt(e)]&&(a=parseInt(b)+parseInt(e),thumbnails[a].focus()):
+"a"===d.key?thumbnails[parseInt(a)-1]&&(a=parseInt(b)-1,thumbnails[a].focus()):"d"===d.key&&thumbnails[parseInt(a)+1]&&(a=parseInt(b)+1,thumbnails[a].focus())},!0)}})();
+function h(){var c=document.querySelectorAll(".yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail");c=Array.from(c);thumbnails=c.filter(function(a){return a.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer")});thumbnails.forEach(function(a){"1"!==a.getAttribute("tabindex")&&(a.setAttribute("tabindex","1"),a.addEventListener("focus",function(){document.activeElement.scrollIntoView({behavior:"instant",block:"center"})}))})}var l=document.getElementById("contents");
+(new MutationObserver(function(){h()})).observe(l,{childList:!0});
 })()
